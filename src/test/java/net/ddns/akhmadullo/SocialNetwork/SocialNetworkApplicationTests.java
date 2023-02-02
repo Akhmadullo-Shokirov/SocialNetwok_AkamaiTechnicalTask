8e742d99eb9bedf564256fa366a680cf8e8bb6a2
Akhmadullo-Shokirov <akhmadullo2001@outlook.com>
2023-02-02 20:25:38 +0100

    Initial setup

---
+++ src/test/java/net/ddns/akhmadullo/SocialNetwork/SocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ddns.akhmadullo.SocialNetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
