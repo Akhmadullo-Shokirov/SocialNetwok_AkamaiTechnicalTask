1636e6a3127eb5422edebe7247bb43dec6b17715
Akhmadullo-Shokirov <akhmadullo2001@outlook.com>
2023-02-03 20:50:27 +0100

    Created PostRepository interface to store/retrieve/update/delete Post entity

---
+++ src/main/java/net/ddns/akhmadullo/SocialNetwork/repository/PostRepository.java
@@ -0,0 +1,8 @@
+package net.ddns.akhmadullo.SocialNetwork.repository;
+
+import net.ddns.akhmadullo.SocialNetwork.entity.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, String> {
+
+}
